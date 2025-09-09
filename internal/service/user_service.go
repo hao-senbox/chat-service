e936587b2089ac46d7289e2b2270f9a43dcb40d6
provided <provided@provideds-Mac-mini.local>
2025-09-09 15:59:11 +0700

    push code

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -121,7 +121,7 @@ func (u *userService) GetUserInfor(ctx context.Context, userID string) (*models.
 
 	return &models.UserInfor{
 		UserID:   safeString(innerData["id"]),
-		UserName: safeString(innerData["username"]),
+		UserName: safeString(innerData["nickname"]),
 		FullName: safeString(innerData["fullname"]),
 		NickName: safeString(innerData["nickname"]),
 		Avartar:  safeString(innerData["avatar"]),
